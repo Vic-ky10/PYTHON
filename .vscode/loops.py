082924957fec3ec6c26fd0f45588deed9d5a1588
vigneshwaran <vigneshvenketasan@gmail.com>
2026-05-18 15:12:19 +0530

    Learned Loops Concepts in Python

---
+++ .vscode/loops.py
@@ -0,0 +1,43 @@
+#loops 
+# While Loops 
+# i = 1 
+# while i<10:
+#     i+= 1
+#     if i == 3:
+#         continue 
+#     print(i)
+# else : 
+#     print("i is no longer less than 10 ")
+
+
+# #FOR LOOPS 
+# fruits = ["apple", "mango", "banana"]
+# for x in fruits :
+#     if x == "mango":
+#         continue
+#     print(x)
+#     for z in "vigneshwaran":
+#         print(z)
+                                    
+#For Loops Range 
+
+
+# for y in range( 2, 5 ):
+#     print(y)
+    
+#     for x in range( 0 , 50 , 5):
+#         print(x)
+
+
+names = ["vicky" , "anbu" , "kamalesh"]
+actions = ['codes', 'eats', 'sleep']
+
+for action in actions:
+    for name in names:
+        print(name + ' '+ action) 
+        
+        for name in names:
+          for action in actions:
+   
+           print(name + ' '+ action)
+        
