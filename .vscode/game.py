ae06360b971fd20c60f3c66579a5b9b22fbe3165
vigneshwaran <vigneshvenketasan@gmail.com>
2026-05-18 14:20:39 +0530

    Finish the Game with enhancement

--- .vscode/game.py
+++ .vscode/game.py
@@ -1,9 +1,16 @@
 
 import random
+import sys
+from enum import Enum
+
+class RPS(Enum):
+    ROCK = 1 
+    PAPER = 2
+    SCISSORS = 3
+
 
 
 print('')
-
 playerchoise = input("Enter.... \n1  for Rock , \n2 for Paper ,\n3 for Scissors:\n\n")
 
 print(playerchoise)
@@ -15,8 +22,8 @@ computerchoise = random.choice('123')
 computer = int(computerchoise)
 
 print("")
-print("You chose " + playerchoise + ".")
-print("Python chose " + computerchoise + ".") 
+print("You chose " + str(RPS(player)).replace("RPS." , "")+ ".")
+print("Python chose " + str(RPS(computer)).replace("RPS." , "") + ".") 
 print("")
 
 if player == 1 and computer == 3:
@@ -30,4 +37,6 @@ elif player == computer :
 else:
     print("Pyton win!")
 
+if player > 3 or player < 1 : 
+  sys.exit(" Enter Number betwen 1 tp 3")
   
